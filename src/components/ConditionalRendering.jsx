60aefad9abdf8204ebfeeeb97979a2f71af52c5e
teatea-zz <lalahyun.d@gmail.com>
2024-04-01 17:41:58 +0900

    pre tutorials before dev

---
+++ src/components/ConditionalRendering.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ConditionalRendering() {
+  const num = 10;
+  const str = '';
+
+  if (num % 2 === 0) {
+    return <div>{num}은 짝수</div>;
+  } else {
+    return <div>{num}은 홀수</div>;
+  }
+
+  return <div></div>;
+}
+
+export default ConditionalRendering;
